03b7186c1e5969ebd0f9de9938bd595fb6de09f8
Klara Miffili <fleischmann.kl@gmail.com>
2020-08-28 13:24:48 +0200

    adapt test set up for integration test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,9 +3,19 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
+  let container;
+
+  beforeEach(() => {
+    container = render(<App />);
+  })
+
   it('renders headline "ToDos"', () => {
-    const { getByText } = render(<App />);
-    const headline = getByText(/ToDos/i);
+    const headline = container.getByText(/ToDos/i);
     expect(headline).toBeInTheDocument();
+  });
+
+  it('includes live feedback for screen readers', () => {
+    const liveFeedback = container.getByRole('status');
+    expect(liveFeedback).toBeInTheDocument();
   });
 });
